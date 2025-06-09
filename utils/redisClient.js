a281f83b00e23cd983173039c89a9bb98b2bc905
Himanshu <himanshuingle42@gmail.com>
2025-06-09 18:05:02 +0530

    chore(redis): update Redis connection URL

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -1,6 +1,8 @@
 const redis = require("redis");
 
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({
+  url: process.env.REDIS_URL,
+});
 
 redisClient.on("error", (err) => console.error("❌ Redis Error:", err));
 
